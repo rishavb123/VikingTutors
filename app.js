c3b00da6d9948d31cfbb42b7a50ccb3e0c6c9853
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-02 17:25:10 -0400

    fixed tabbing

--- app.js
+++ app.js
@@ -21,16 +21,16 @@ $(document).ready(() => {
 
 $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
     if (!$(this).next().hasClass('show')) {
-      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+        $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
     }
-    var $subMenu = $(this).next('.dropdown-menu');
+    let $subMenu = $(this).next('.dropdown-menu');
     $subMenu.toggleClass('show');
-  
-  
+
+
     $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-      $('.dropdown-submenu .show').removeClass('show');
+        $('.dropdown-submenu .show').removeClass('show');
     });
-  
-  
+
+
     return false;
-  });
+});
