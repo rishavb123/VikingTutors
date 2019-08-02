0eed68ee4a0b1cfdf18f67dc83062ccb818dd2a6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-02 17:03:43 -0400

    tried to get sub dropdowns to work

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $(document).ready(() => {
         querySnapshot.forEach(async (doc) => {
             getVideoData(doc.data().videoId, (data) => {
                 data.embed = new Handlebars.SafeString(data.embed);
+                console.log(data);
                 data.description = new Handlebars.SafeString(anchorme(data.description.replace(/\n/g,"<br />")));
                 const html = template(data);
                 $("#test").append($(html).attr("class", "video"));
@@ -16,3 +17,25 @@ $(document).ready(() => {
     });
 
 });
+
+// $(document).on('click', '.dropdown-item', function (e) {
+//     console.log($(e.target).hasClass('dropdown-item'));
+//     if(!$(e.target).hasClass('dropdown-item'))
+//         e.stopPropagation();
+// });
+
+$('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+    if (!$(this).next().hasClass('show')) {
+      $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
+    }
+    var $subMenu = $(this).next('.dropdown-menu');
+    $subMenu.toggleClass('show');
+  
+  
+    $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+      $('.dropdown-submenu .show').removeClass('show');
+    });
+  
+  
+    return false;
+  });
