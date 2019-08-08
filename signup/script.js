6a6fd33df12db6afbf1499784a42e4c3ee4ebe50
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-08 17:37:25 -0400

    created sign up page

---
+++ signup/script.js
@@ -0,0 +1,17 @@
+$('.btn').click(() => {
+    let email = $('#emailField').val();
+    let password = $('#passwordField').val();
+    let confirmPassword = $('#confirmPasswordField').val();
+    if(password !== confirmPassword)
+        if(email.split("@")[1] === "sbstudents.org" || email.split("@")[1] === "sbschools.org" || email.split("@")[1] === "bhagat.io")
+            firebase.auth().createUserWithEmailAndPassword(email, password);
+        else
+            alert("Please use your SBHS email address");
+    else
+        alert("Confirm Password must match Password");
+});
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user)
+        location.href="../index.html"
+});
