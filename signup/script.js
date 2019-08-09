e09759789e9b76a834646cf3354f7f85bba3c270
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-08 22:46:55 -0400

    added name fields

--- signup/script.js
+++ signup/script.js
@@ -1,10 +1,17 @@
 $('.btn').click(() => {
     let email = $('#emailField').val();
     let password = $('#passwordField').val();
+    let first = $('#firstField').val();
+    let last = $('#lastField').val();
+
     let confirmPassword = $('#confirmPasswordField').val();
     if(password === confirmPassword)
         if(email.split("@")[1] === "sbstudents.org" || email.split("@")[1] === "sbschools.org" || email.split("@")[1] === "bhagat.io")
-            firebase.auth().createUserWithEmailAndPassword(email, password);
+            firebase.auth().createUserWithEmailAndPassword(email, password).then(user => {
+                return user.updateProfile({
+                    displayName: first + " " + last
+                });
+            });
         else
             alert("Please use your SBHS email address");
     else
