a1084004b679ce31e0e88bd15ee10683a44bd8e2
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-09 09:41:19 -0400

    removed names

--- signup/script.js
+++ signup/script.js
@@ -1,17 +1,11 @@
 $('.btn').click(() => {
     let email = $('#emailField').val();
     let password = $('#passwordField').val();
-    let first = $('#firstField').val();
-    let last = $('#lastField').val();
 
     let confirmPassword = $('#confirmPasswordField').val();
     if(password === confirmPassword)
         if(email.split("@")[1] === "sbstudents.org" || email.split("@")[1] === "sbschools.org" || email.split("@")[1] === "bhagat.io")
-            firebase.auth().createUserWithEmailAndPassword(email, password).then(user => {
-                return user.updateProfile({
-                    displayName: first + " " + last
-                });
-            });
+            firebase.auth().createUserWithEmailAndPassword(email, password);
         else
             alert("Please use your SBHS email address");
     else
