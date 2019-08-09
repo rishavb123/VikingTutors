48dd28a42feb3d5618fa776a53a2626515b35a51
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-09 12:42:45 -0400

    file restructuring

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const db = firebase.firestore();
-
 $(document).ready(() => {
     
     const source = $("#video-template")[0].innerHTML;
@@ -16,79 +14,4 @@ $(document).ready(() => {
         });
     });
 
-});
-
-firebase.auth().onAuthStateChanged(user => {
-    if(user) {
-        let domain = firebase.auth().currentUser.email.split("@")[1];
-        if(domain !== "sbstudents.org" && domain !== "sbschools.org" && domain !== "bhagat.io")
-            firebase.auth().signOut();
-    }
-    else
-        location.href="login/index.html";
-});
-
-db.collection("topics").get().then((querySnapshot) => {
-    let topics = {};
-    querySnapshot.forEach((doc) => {
-        let temp = doc.data();
-        if(temp.superTopic)
-            temp.superTopic = temp.superTopic.path;
-        topics[doc.ref.path] = temp;
-    });
-    let hierarchy = {};
-    const get = (path) => {
-        if(topics[path].superTopic) {
-            let superVal = get(topics[path].superTopic);
-            return superVal? superVal.subtopics[path]: null;
-        }
-        return hierarchy[path];
-    };
-    const set = (path) => {
-        if(get(path)) return;
-        let val = {...topics[path]};
-        val.subtopics = {};
-        val.displayed = false;
-        if(!val.superTopic)
-            hierarchy[path] = val;
-        else {
-            let obj = get(val.superTopic);
-            if(obj) {
-                obj.subtopics[path] = val;
-            } else {
-                set(val.superTopic);
-                set(path)
-            }
-        }
-    };
-
-    for(let path in topics) {
-        set(path);
-    }
-
-    const source = $("#dropdown-item-template")[0].innerHTML;
-    const dropDownItem = Handlebars.compile(source);
-
-    const source2 = $("#dropdown-submenu-template")[0].innerHTML;
-    const dropDownSubMenu = Handlebars.compile(source2);
-
-    const display = (menuId, obj) => {
-        for(let path in obj) {
-            let val = get(path);
-            if(Object.keys(val.subtopics).length == 0) {
-                $("#" + menuId).append(dropDownItem(val));
-            } else {
-                const id = path.replace("s/", "-");
-                $("#" + menuId).append(dropDownSubMenu({
-                    name: val.name,
-                    id
-                }));
-                display(id, val.subtopics);
-            }
-        }
-    };
-
-    display('topic-root', hierarchy);
-    $(".dropdown-toggle").dropdown();
-    $('.dropdown-toggle').dropdownHover();
 });
---
+++ res/js/auth.js
@@ -0,0 +1,9 @@
+firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+        let domain = firebase.auth().currentUser.email.split("@")[1];
+        if(domain !== "sbstudents.org" && domain !== "sbschools.org" && domain !== "bhagat.io")
+            firebase.auth().signOut();
+    }
+    else
+        location.href="login/index.html";
+});
