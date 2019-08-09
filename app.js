c70ac5b1146101865ad4b5df98473ef30a753b36
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-08 22:27:07 -0400

    added more domain restrictions

--- app.js
+++ app.js
@@ -18,6 +18,16 @@ $(document).ready(() => {
 
 });
 
+firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+        let domain = firebase.auth().currentUser.email.split("@")[1];
+        if(domain !== "sbstudents.org" || domain !== "sbschools.org" || domain !== "bhagat.io")
+            firebase.auth().signOut();
+    }
+    else
+        location.href="login/index.html";
+});
+
 db.collection("topics").get().then((querySnapshot) => {
     let topics = {};
     querySnapshot.forEach((doc) => {
