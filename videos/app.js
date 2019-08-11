eadaf1b35e1b42771ad3114245189f5753add75d
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-10 20:24:09 -0400

    created videos page

---
+++ videos/app.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    
+    const source = $("#video-template")[0].innerHTML;
+    const template = Handlebars.compile(source);
+
+    db.collection("videos").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            getVideoData(doc.data().videoId, (data) => {
+                data.embed = new Handlebars.SafeString(data.embed);
+                data.description = new Handlebars.SafeString(anchorme(data.description.replace(/\n/g,"<br />")));
+                const html = template(data);
+                $("#test").append($(html).attr("class", "video"));
+            });
+        });
+    });
+
+});
