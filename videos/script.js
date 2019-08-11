40c064033ae534a1b8756fdc349bb3ca297f8fcf
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-11 00:38:33 -0400

    got sub/super topic search working

--- videos/script.js
+++ videos/script.js
@@ -6,14 +6,26 @@ function onNavReady(querySnapshot, hierarchy, topics) {
     console.log(params);
     console.log(hierarchy);
 
-    db.collection("videos").where("topics", "array-contains", params.t).get().then((querySnapshot) => {
-        console.log(querySnapshot.size);
-        querySnapshot.forEach((doc) => {
-            getVideoData(doc.data().videoId, (data) => {
-                data.embed = new Handlebars.SafeString(data.embed);
-                data.description = new Handlebars.SafeString(anchorme(data.description.replace(/\n/g,"<br />")));
-                const html = template(data);
-                $("#test").append($(html).attr("class", "video"));
+    let selectedTopics = [];
+    let searchTopic = getFromHierarchy("topics/" + params.t, hierarchy, topics);
+    console.log(searchTopic);
+    const select = (topic, path) => {
+        selectedTopics.push(path.split("/")[1]);
+        for(let subPath in topic.subtopics) {
+            select(topic.subtopics[subPath], subPath);
+        }
+    }
+    select(searchTopic, "topics/" + params.t);
+    console.log(selectedTopics);
+    Promise.all(selectedTopics.map(topic => db.collection("videos").where("topics", "array-contains", topic).get())).then((querySnapshots) => {
+        querySnapshots.forEach((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                getVideoData(doc.data().videoId, (data) => {
+                    data.embed = new Handlebars.SafeString(data.embed);
+                    data.description = new Handlebars.SafeString(anchorme(data.description.replace(/\n/g,"<br />")));
+                    const html = template(data);
+                    $("#test").append($(html).attr("class", "video"));
+                });
             });
         });
     });
