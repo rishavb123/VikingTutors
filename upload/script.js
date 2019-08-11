1a39e4094165e088e5c046bccb4ec700e537ae5c
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-11 16:07:19 -0400

    got topic system working

--- upload/script.js
+++ upload/script.js
@@ -1,6 +1,43 @@
-const navResults = {}
+let selectedTopicPath = "";
+let navResults;
+
+// <select class="form-control" id="topicInput">
+//     <option>Mr. </option>
+//     <option>Mrs. </option>
+//     <option>Ms. </option>
+//     <option>Dr. </option>
+//     <option>Miss </option>
+// </select>
+
+function name() {
+    console.log(getFromHierarchy(selectedTopicPath, navResults.hierarchy, navResults.topics).name);
+}
+setInterval(name, 1000);
 
 function onNavReady(querySnapshot, hierarchy, topics) {
-    navResults.hierarchy = hierarchy;
-    navResults.topics = topics;
+    navResults = { querySnapshot, hierarchy, topics };
+
+
+    updateUI();
+}
+
+function updateUI() {
+    let topicId = selectedTopicPath.split("/")[1] || "root";
+    let nextTopics = getFromHierarchy(selectedTopicPath, navResults.hierarchy, navResults.topics).subtopics;
+    if(Object.values(nextTopics).length > 0) {
+        $('#topic-group').append(`<select class="form-control" id=select_${topicId} selectedIndex=-1><option class="empty" id=option_${topicId} selected></option></select>`);
+        for(let path in nextTopics) {
+            let curtopicId = path.split("/")[1];
+            $('#select_' + topicId).append(`<option id=option_${curtopicId}>${getFromHierarchy(path, navResults.hierarchy, navResults.topics).name}</option>`);
+        }
+    }
+
+    $("select").change(function(event) {
+        let e = event.target;
+        selectedTopicPath = "topics/" + e.options[e.selectedIndex].id.split("_")[1];
+        $(e).nextAll('select').remove();
+        if(!$(e.options[e.selectedIndex]).hasClass("empty"))
+            updateUI();
+    });
+
 }
