89cb8c7812aef571a41fd752d50956e4380c47b6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-11 20:30:24 -0400

    added ability to select multiple topics

--- upload/script.js
+++ upload/script.js
@@ -1,35 +1,42 @@
-let selectedTopicPath = "";
+let selectedTopicPath = [""];
 let navResults;
+let numOfTopics = 0;
 
-// function name() {
-//     console.log(getFromHierarchy(selectedTopicPath, navResults.hierarchy, navResults.topics).name);
-// }
-// setInterval(name, 1000);
+function name() {
+    console.log(selectedTopicPath.map(s => getFromHierarchy(s, navResults.hierarchy, navResults.topics).name));
+}
+setInterval(name, 1000);
 
 function onNavReady(querySnapshot, hierarchy, topics) {
     navResults = { querySnapshot, hierarchy, topics };
 
-
-    updateUI();
+    updateUI(numOfTopics);
 }
 
-function updateUI() {
-    let topicId = selectedTopicPath.split("/")[1] || "root";
-    let nextTopics = getFromHierarchy(selectedTopicPath, navResults.hierarchy, navResults.topics).subtopics;
+function updateUI(topicIndex) {
+    let topicId = selectedTopicPath[topicIndex].split("/")[1] || "root";
+    let nextTopics = getFromHierarchy(selectedTopicPath[topicIndex], navResults.hierarchy, navResults.topics).subtopics;
     if(Object.values(nextTopics).length > 0) {
-        $('#topic-group').append(`<select class="form-control" id=select_${topicId} selectedIndex=-1><option class="empty" id=option_${topicId} selected></option></select>`);
+        $('#topic-' + topicIndex).append(`<select class="form-control select_${topicIndex}" id=select_${topicIndex}_${topicId} selectedIndex=-1><option class="empty" id=option_${topicIndex}_${topicId} selected></option></select>`);
         for(let path in nextTopics) {
             let curtopicId = path.split("/")[1];
-            $('#select_' + topicId).append(`<option id=option_${curtopicId}>${getFromHierarchy(path, navResults.hierarchy, navResults.topics).name}</option>`);
+            $(`#select_${topicIndex}_` + topicId).append(`<option id=option_${topicIndex}_${curtopicId}>${getFromHierarchy(path, navResults.hierarchy, navResults.topics).name}</option>`);
         }
     }
 
-    $("select").change(function(event) {
+    $(".select_" + topicIndex).change(function(event) {
         let e = event.target;
-        selectedTopicPath = "topics/" + e.options[e.selectedIndex].id.split("_")[1];
+        selectedTopicPath[topicIndex] = "topics/" + e.options[e.selectedIndex].id.split("_")[2];
         $(e).nextAll('select').remove();
         if(!$(e.options[e.selectedIndex]).hasClass("empty"))
-            updateUI();
+            updateUI(topicIndex);
     });
 
 }
+
+$('#addTopicButton').click(() => {
+    numOfTopics++;
+    selectedTopicPath.push("");
+    $(".topic-container-container").append(`<div class="topic-container" id="topic-${numOfTopics}"></div>`);
+    updateUI(numOfTopics);
+});
