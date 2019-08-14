51ae5dff1dfbe861cea6c372222b037919b8d6ce
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-14 15:07:56 -0400

    added basic frontend for home page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 new Rellax(".rellax");
+
+$(document).ready(function() {
+    $(".about-title").css("height", $(".about-text").height());
+});
+$(window).resize(function() {
+    $(".about-title").css("height", $(".about-text").height());
+});
