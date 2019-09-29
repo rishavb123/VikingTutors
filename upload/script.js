d08fbcef22f2f4d14f3d2b517daae9b16ac185ff
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-29 17:05:27 -0400

    added ability to upload presentations

--- upload/script.js
+++ upload/script.js
@@ -1,11 +1,14 @@
 let selectedTopicPath = ["topics/root"];
+let selectedTopicPath2 = ["topics/root"];
 let navResults;
 let numOfTopics = 0;
+let numOfTopics2 = 0;
 
 function onNavReady(querySnapshot, hierarchy, topics) {
     navResults = { querySnapshot, hierarchy, topics };
 
     updateUI(numOfTopics);
+    updateUI2(numOfTopics);
 }
 
 function updateUI(topicIndex) {
@@ -50,11 +53,60 @@ function updateUI(topicIndex) {
 
 }
 
+function updateUI2(topicIndex) {
+    let topicId = selectedTopicPath2[topicIndex].split("/")[1] || "root";
+    let curTopic = getFromHierarchy(selectedTopicPath2[topicIndex], navResults.hierarchy, navResults.topics);
+    let nextTopics = curTopic.subtopics;
+    $('#topic2-' + topicIndex).append(`<select class="form-control select2_${topicIndex}" id=select2_${topicIndex}_${topicId} selectedIndex=-1><option id=option2_${topicIndex}_${topicId} selected></option></select>`);
+    for(let path in nextTopics) {
+        let curtopicId = path.split("/")[1];
+        $(`#select2_${topicIndex}_` + topicId).append(`<option id=option2_${topicIndex}_${curtopicId}>${getFromHierarchy(path, navResults.hierarchy, navResults.topics).name}</option>`);
+    }
+    $(`#select2_${topicIndex}_` + topicId).append(`<span id="newtopic2_${topicIndex}"></span><option id=option2_${topicIndex}_create>Create New Sub Topic of ${curTopic.name}</option>`);
+
+    $(".select2_" + topicIndex).change(function(event) {
+        let e = event.target;
+        if(e.selectedIndex === e.options.length - 1) {
+            let name = prompt(`What is the name of the new sub topic of ${curTopic.name}`);
+            if(name) {
+                let newTopicId = db.collection("topics").doc().id;
+                let obj = { name };
+                if(selectedTopicPath2[topicIndex] !== "topics/root")
+                    obj.superTopic = db.doc(selectedTopicPath2[topicIndex]);
+                getFromHierarchy(selectedTopicPath2[topicIndex], navResults.hierarchy, navResults.topics).subtopics[`topics/${newTopicId}`] = {
+                    name,
+                    subtopics: {}
+                };
+                navResults.topics[`topics/${newTopicId}`] = { name, superTopic: obj.superTopic? obj.superTopic.path : undefined };
+                console.log(obj);
+                db.collection("topics").doc(newTopicId).set(obj);
+            } else {
+                e.selectedIndex = 0;
+            }
+        }
+        $(e).nextAll('select').remove();
+        if(e.selectedIndex !== e.options.length - 1)
+            selectedTopicPath2[topicIndex] = "topics/" + e.options[e.selectedIndex].id.split("_")[2];
+        else
+            $(e).remove();
+        if(e.selectedIndex !== 0)
+            updateUI2(topicIndex);
+    });
+
+}
+
 $('#addTopicButton').click(() => {
     numOfTopics++;
     selectedTopicPath.push("");
     $(".topic-container-container").append(`<div class="topic-container" id="topic-${numOfTopics}"></div>`);
     updateUI(numOfTopics);
+});
+
+$('#addTopicButton2').click(() => {
+    numOfTopics2++;
+    selectedTopicPath2.push("");
+    $(".topic-container-container-2").append(`<div class="topic-container" id="topic2-${numOfTopics2}"></div>`);
+    updateUI2(numOfTopics2);
 });
 
 $(".upload-button").click(() => {
@@ -63,6 +115,18 @@ $(".upload-button").click(() => {
             videoId: getUrlParametersFromUrl($('#linkInput').val()).v,
             teacher: querySnapshot.docs[0].id,
             topics: selectedTopicPath.map(s => s.split("/")[1]),
+        }).then(() => location.reload());
+    });
+});
+
+$(".upload-button-2").click(() => {
+    db.collection("teachers").where("uid", "==", firebase.auth().currentUser.uid).get().then((querySnapshot) => {
+        db.collection("gdrivefiles").doc().set({
+            name: $("#nameInput").val(),
+            description: $("#descriptionInput").val(),
+            url: $($("#embedCodeInput").val()).attr("src"),
+            teacher: querySnapshot.docs[0].id,
+            topics: selectedTopicPath2.map(s => s.split("/")[1])
         }).then(() => location.reload());
     });
 });
