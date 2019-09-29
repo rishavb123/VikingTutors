cdc57285afb9216e119f7f9fac64c3d64ee22322
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-29 17:37:43 -0400

    created basic gdfile viewer

--- file/script.js
+++ file/script.js
@@ -1,31 +1,20 @@
 function onNavReady(querySnapshot, hierarchy, topics) {
-    const source = $("#video-template")[0].innerHTML;
+    const source = $("#gdfile-template")[0].innerHTML;
     const template = Handlebars.compile(source);
 
     let params = getUrlParameters();
 
-    db.collection("videos").where("videoId", "==", params.v).get().then((querySnapshot) => {
-        let doc = querySnapshot.docs[0];
-        let videoData = doc.data();
-        db.collection("teachers").doc(videoData.teacher).get().then(teacherDoc => {
+    db.collection("gdrivefiles").doc(params.f).get().then((doc) => {
+        let fileData = doc.data();
+        db.collection("teachers").doc(fileData.teacher).get().then(teacherDoc => {
             let teacher = teacherDoc.data();
-            getVideoData(videoData.videoId, (data) => {
-                data.embed = new Handlebars.SafeString(data.embed);
-                data.description = new Handlebars.SafeString(anchorme(data.description.replace(/\n/g,"<br />"), {
-                    attributes: [{
-                        name: "class",
-                        value: "anchorme-link"
-                    }]
-                }));
-                data.id = videoData.videoId;
-                data.teacher = teacher.honorific + teacher.name.last;
-                const html = template(data);
-                $("#video-container").append(html);
-            });
+            let data = {};
+            data.name = fileData.name
+            data.description = new Handlebars.SafeString(anchorme(fileData.description));
+            data.url = fileData.url;
+            data.teacher = teacher.honorific + teacher.name.last;
+            const html = template(data);
+            $("#gdfile-container").append(html);
         });
     });
-}
-
-function openVideo(e) {
-    console.log($(e.target.parentNode).attr('id'));
 }
