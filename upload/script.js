6ef14e4e58d004ca4bc76a8c6bb62167d1728d4c
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-29 22:33:43 -0400

    added need help videos

--- upload/script.js
+++ upload/script.js
@@ -137,6 +137,9 @@ const template = Handlebars.compile(source);
 const source2 = $("#gdfile-template")[0].innerHTML;
 const template2 = Handlebars.compile(source2);
 
+const source3 = $("#video-template-2")[0].innerHTML;
+const template3 = Handlebars.compile(source3);
+
 function onAuthStateChanged(user, domain) {
     
     if(domain === "sbstudents.org")
@@ -208,6 +211,43 @@ function onAuthStateChanged(user, domain) {
                             return titleA < titleB? -1 : titleA > titleB? 1 : 0;
                         });
                         $('#video-container').append(items);
+                    });
+                });
+            });
+        });
+
+        db.collection("videos").where("teacher", "==", "8qTD9qDBzB8KZO8IICE2").get().then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                let videoData = doc.data();
+                db.collection("teachers").doc(videoData.teacher).get().then(teacherDoc => {
+                    let teacher = teacherDoc.data();
+                    getVideoData(videoData.videoId, (data) => {
+                        data.embed = new Handlebars.SafeString(data.embed);
+                        data.description = new Handlebars.SafeString(anchorme(data.description.replace(/\n/g,"<br />"), {
+                            attributes: [{
+                                name: "class",
+                                value: "anchorme-link"
+                            }]
+                        }));
+                        function toTitleCase(str) {
+                            return str.replace(
+                                /\w\S*/g,
+                                function(txt) {
+                                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                                }
+                            );
+                        }
+                        data.id = videoData.videoId;
+                        data.teacher = (teacher.honorific && teacher.name && teacher.name.last)? teacher.honorific + teacher.name.last : toTitleCase(teacher.email.split('.')[0] + " " + teacher.email.split("@")[0].split('.')[1]);
+                        data.vtId = doc.id;
+                        const html = template3(data);
+                        $("#tut-video-container").append(html);
+                        let items = $('#tut-video-container').children().sort((a, b) => {
+                            titleA = $(a).children('h1')[0].innerHTML; 
+                            titleB = $(b).children('h1')[0].innerHTML; 
+                            return titleA < titleB? -1 : titleA > titleB? 1 : 0;
+                        });
+                        $('#tut-video-container').append(items);
                     });
                 });
             });
